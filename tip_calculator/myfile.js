6efada5d738f07b8a7bb3fd3f3c18a3c02692864
Amanuel85 <40584879+Avunue@users.noreply.github.com>
2021-03-15 21:15:28 -0500

    update

--- tip_calculator/myfile.js
+++ tip_calculator/myfile.js
@@ -18,16 +18,21 @@ function reset(){
 window.onload = pageLoad
 */
 function pageLoad(){
-    document.getElementById("btn").onclick = calcTip;
+    document.getElementById("tip").onclick = calcTip;
 }
 function calcTip() {
 	//let subtotalElem = 
 	//var tipElem = 
 	//var totalElem = document.getElementById('total');
-	let  subtotal = document.getElementById("subtotal").value
-	let tip =  document.getElementById("tip").value
-    let total = parseFloat(subtotal+ subtotal*tip/100)
-    document.getElementById("total").value = total
+	 subtotal = document.getElementById("subtotal")
+    let subValue = +subtotal.value;
+    
+	tip =  document.getElementById("tip")
+    
+   let tipvalue = +tip.value
+   // let total = parseFloat(subtotal+ subtotal*tip/100)
+   let totalWtip = subValue + subValue*tipvalue*0.01
+    document.getElementById("total").value = totalWtip
 	//totalElem.innerHTML = '$' + total;
 }
-window.onload = pageLoad
+window.onload = pageLoad;
--- tip_calculator/test.js
+++ tip_calculator/test.js
@@ -1,1 +1,38 @@
-for (let i = 1; i <= 2; i++) { for (let j = 0; j <= 4; j = j + 2) { console.log(i * j) } }
+"use strict"
+"ESLint-disable"
+
+function pageLoad() { document.getElementById("go").onclick = goClick; }
+
+function goClick() {
+    setTimeout(countDown, 1000);
+}
+
+function alarm() {
+    const bodyElement = document.getElementById("body");
+    bodyElement.className = "awesome";
+}
+
+/**
+ * make the textbox count down the seconds
+ * get the current value and reduce it to 0
+ * if value is 0 cancel the timer
+ */
+function countDown() {
+   let secondsTextbox = document.getElementById("sec");
+    const seconds = +secondsTextbox.value;
+    let minuteTextbox = document.getElementById("min");
+    let minutes = +minuteTextbox.value
+    if(minutes > 0){
+        minuteTextbox.value = 60*seconds-1
+        //setTimeout(countDown,1000*60*minutes)
+    }
+
+    if (seconds > 0) {
+        secondsTextbox.value = seconds - 1;
+        setTimeout(countDown, 1000);
+    } else {
+        alarm();
+    }
+}
+
+window.onload = pageLoad;
